ce731fbd4405e1ab01ec457254c6a9efee708676
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-17 20:47:42 +0330

    feat: add Api service & GetFact handler & JSON API

---
+++ api.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type ApiServer struct {
+	svc Service
+}
+
+func NewApiServer(svc Service) *ApiServer {
+	return &ApiServer{
+		svc: svc,
+	}
+}
+
+func (s *ApiServer) Start(listenAddr string) error {
+	http.HandleFunc("/", s.handleGetCatFact)
+	return http.ListenAndServe(listenAddr, nil)
+}
+
+func (s *ApiServer) handleGetCatFact(w http.ResponseWriter, r *http.Request) {
+	fact, err := s.svc.GetCatFact(context.Background())
+	if err != nil {
+		writeJSON(w, http.StatusUnprocessableEntity, map[string]any{"error": err.Error()})
+		return
+	}
+	writeJSON(w, http.StatusOK, fact)
+}
+func writeJSON(w http.ResponseWriter, s int, v any) error {
+	w.WriteHeader(s)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
