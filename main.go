03fdc6b5c8078726364695cecaa5f6ee54ef6e92
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-17 20:48:11 +0330

    feat: run api on port :3000

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 )
 
 func main() {
 	svc := NewCatFactService("https://catfact.ninja/fact")
 	svc = NewLoggingService(svc)
+	apiServer := NewApiServer(svc)
+	log.Fatal(apiServer.Start(":3000"))
 
-	fact, err := svc.GetCatFact(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", fact)
 }
